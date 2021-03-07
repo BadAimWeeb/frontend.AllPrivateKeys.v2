25f9ee743ec92eb3c100e911cfa0b5d0b4082466
BadAimWeeb <29724654+BadAimWeeb@users.noreply.github.com>
2021-03-07 16:53:44 +0700

    ?

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 export default {
     defaultServers: [
-		{
-			location: "https://s1-v2.allprivatekeys.org/apksocket",
+        {
+            location: "https://s1-v2.allprivatekeys.org/apksocket",
             name: "APKv2 Server 1",
             mode: "socket"
-		},
-		{
-			location: "https://s2-v2.allprivatekeys.org/apksocket",
+        },
+        {
+            location: "https://s2-v2.allprivatekeys.org/apksocket",
             name: "APKv2 Server 2",
             mode: "socket"
-		},
+        },
         {
             location: "http://localhost:3000/apksocket",
             name: "Default local server",
